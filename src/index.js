22f0a849eca6ba3d7771880c4dd667cafa33646e
Denis <gold8523@gmail.com>
2022-04-09 16:20:01 +0300

    homework-1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './components/Header';
+import Slider from './components/Slider';
+import Footer from './components/Footer';
+// import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Slider />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
