483ca6191e3fa7ba45e1b223926426a63438ddf0
Denis <gold8523@gmail.com>
2022-05-31 19:35:10 +0300

    доработал форму и немного поправил вёрстку

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -71,69 +71,82 @@ const LoginPage = () => {
         [s.active]: active
         })}>
         <div className={s.card}></div>
-        <div className={cn(s.card, s.alt)}>
-          <Heading level={1} className={s.title}>
-            Login
-          </Heading>
-          <form 
-            ref={ref}
-            name="loginForm"
-            onSubmit={handleFormSubmit}
-          >
-            <div className={s.inputContainer}>
-              <Input type="email" name="email" required value={form.email} onChange={handleChangeInput}/>
-              <label htmlFor="#email">Email</label>
-              <div className={s.bar}></div>
+        {
+          active ?
+          <div className={cn(s.card, s.alt)}>
+            <div className={cn(s.toggle, {
+                [s.active]: active
+              })} onClick={handleToggleForm}>
+              {active ? null : <Pen />}
             </div>
-            <div className={s.inputContainer}>
-              <Input type="password" name="password" required value={form.password} onChange={handleChangeInput}/>
-              <label htmlFor="#password">Password</label>
-              <div className={s.bar}></div>
-            </div>
-            <div className={s.buttonContainer}>
-              <Button type="submit">  
-                Go
-              </Button>
-            </div>
-          </form>
-        </div>
-        <div className={cn(s.card, s.alt)}>
-          {/* <div className={cn(s.toggle, {
-            [s.active]: active
-          })} onClick={handleToggleForm}>
-            <Pen />
-          </div> */}
-          <Heading level={1} className={s.title}>
-            Register
-            <div className={s.close} onClick={handleToggleForm}></div>
-          </Heading>
-          <form 
-            ref={ref}
-            name="registerForm"
-            onSubmit={handleFormSubmit}
-          >
-            <div className={s.inputContainer}>
-              <Input type="email" name="email" required value={form.email} onChange={handleChangeInput}/>
-              <label htmlFor="#signup-email">Email</label>
-              <div className={s.bar}></div>
-            </div>
-            <div className={s.inputContainer}>
-              <Input type="password" name="password" required value={form.password} onChange={handleChangeInput}/>
-              <label htmlFor="#signup-password">Password</label>
-              <div className={s.bar}></div>
-            </div>
-            <div className={s.inputContainer}>
-              <Input type="password" name="repeat" required value={form.repeat} onChange={handleChangeInput}/>
-              <label htmlFor="#signup-repeat-password">Repeat Password</label>
-              <div className={s.bar}></div>
-            </div>
-            <div className={s.buttonContainer}>
-              <Button type="submit">
+              <Heading level={1} className={s.title}>
                 Register
-              </Button>
+                <div className={s.close} onClick={handleToggleForm}></div>
+              </Heading>
+              <form 
+                ref={ref}
+                name="registerForm"
+                onSubmit={handleFormSubmit}
+              >
+                <div className={s.inputContainer}>
+                  <Input type="email" name="email" required value={form.email} onChange={handleChangeInput}/>
+                  <label htmlFor="#signup-email">Email</label>
+                  <div className={s.bar}></div>
+                </div>
+                <div className={s.inputContainer}>
+                  <Input type="password" name="password" required value={form.password} onChange={handleChangeInput}/>
+                  <label htmlFor="#signup-password">Password</label>
+                  <div className={s.bar}></div>
+                </div>
+                <div className={s.inputContainer}>
+                  <Input type="password" name="repeat" required value={form.repeat} onChange={handleChangeInput}/>
+                  <label htmlFor="#signup-repeat-password">Repeat Password</label>
+                  <div className={s.bar}></div>
+                </div>
+                <div className={s.buttonContainer}>
+                  <Button type="submit">
+                    Register
+                  </Button>
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
+            :
+            <div className={cn(s.card, {
+              [s.alt]: active
+            })}>
+              <div className={cn(s.card, s.alt)}>
+                <div className={cn(s.toggle, {
+                  [s.active]: active
+                })} onClick={handleToggleForm}>
+                <Pen />
+              </div>
+            </div>
+              <Heading level={1} className={s.title}>
+                Login
+              </Heading>
+              <form 
+                ref={ref}
+                name="loginForm"
+                onSubmit={handleFormSubmit}
+              >
+                <div className={s.inputContainer}>
+                  <Input type="email" name="email" required value={form.email} onChange={handleChangeInput}/>
+                  <label htmlFor="#email">Email</label>
+                  <div className={s.bar}></div>
+                </div>
+                <div className={s.inputContainer}>
+                  <Input type="password" name="password" required value={form.password} onChange={handleChangeInput}/>
+                  <label htmlFor="#password">Password</label>
+                  <div className={s.bar}></div>
+                </div>
+                <div className={s.buttonContainer}>
+                  <Button type="submit">  
+                    Go
+                  </Button>
+                </div>
+              </form>
+            </div>
+        }
       </div>
       <Footer />
     </div>
